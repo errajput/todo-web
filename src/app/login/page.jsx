e83bf036f174906ab9c8c8db1b9ccf1d7708bbca
disha rajput <dishacsevira@gmail.com>
2025-09-14 20:10:13 +0530

    fix formatting

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -22,7 +22,9 @@ export default function LoginPage() {
     try {
       const res = await fetch("http://localhost:5000/auth/login", {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        headers: {
+          "Content-Type": "application/json",
+        },
         body: JSON.stringify(formData),
       });
 
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -23,7 +23,9 @@ export default function RegisterPage() {
     try {
       const res = await fetch("http://localhost:5000/auth/register", {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
+        headers: {
+          "Content-Type": "application/json",
+        },
         body: JSON.stringify(formData),
       });
 
