4b8a357caa94f5b84595f60678d8fd2702fb58a6
disha rajput <dishacsevira@gmail.com>
2025-09-27 17:26:58 +0530

    Change Input email and password Style

--- src/ui/EmailField.jsx
+++ src/ui/EmailField.jsx
@@ -7,10 +7,10 @@ const EmailField = ({ value, onChange }) => {
         placeholder="Enter your email"
         value={value}
         onChange={onChange}
-        className="peer w-full px-5 py-3 rounded-xl border border-white/40 bg-white/50 placeholder-transparent focus:border-purple-500 focus:ring-1 focus:ring-purple-400 focus:outline-none transition duration-300"
+        className="peer w-full px-5 py-3 rounded-xl border border-gray-400  placeholder-transparent focus:border-purple-500 focus:ring-1 focus:ring-purple-500 focus:outline-none transition duration-300 focus:text-purple-500 text-gray-500"
         required
       />
-      <label className="absolute left-5 -top-2.5 text-purple-600 text-sm font-medium transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-gray-400 peer-placeholder-shown:text-base peer-focus:-top-2.5 peer-focus:text-purple-600 peer-focus:text-sm">
+      <label className="absolute left-5 -top-2.5 text-gray-500 focus:text-purple-600 text-sm font-medium transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-gray-400 peer-placeholder-shown:text-base peer-focus:-top-2.5 peer-focus:text-purple-600 peer-focus:text-sm bg-white px-1">
         Email
       </label>
     </div>
