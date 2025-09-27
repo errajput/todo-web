f7aa36401fa59bb7ecbf014b09a36676b3737c8b
disha rajput <dishacsevira@gmail.com>
2025-09-27 17:50:26 +0530

    Using Id and HtmlFor id

--- src/ui/EmailField.jsx
+++ src/ui/EmailField.jsx
@@ -1,4 +1,7 @@
+import { useId } from "react";
+
 const EmailField = ({ value, onChange }) => {
+  const id = useId();
   return (
     <div>
       <input
@@ -9,8 +12,12 @@ const EmailField = ({ value, onChange }) => {
         onChange={onChange}
         className="peer w-full px-5 py-3 rounded-xl border border-gray-400  placeholder-transparent focus:border-purple-500 focus:ring-1 focus:ring-purple-500 focus:outline-none transition duration-300 focus:text-purple-500 text-gray-500"
         required
+        id={id}
       />
-      <label className="absolute left-5 -top-2.5 text-gray-500 focus:text-purple-600 text-sm font-medium transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-gray-400 peer-placeholder-shown:text-base peer-focus:-top-2.5 peer-focus:text-purple-600 peer-focus:text-sm bg-white px-1">
+      <label
+        htmlFor={id}
+        className="absolute left-5 -top-2.5 text-gray-500 focus:text-purple-600 text-sm font-medium transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-gray-400 peer-placeholder-shown:text-base peer-focus:-top-2.5 peer-focus:text-purple-600 peer-focus:text-sm bg-white px-1"
+      >
         Email
       </label>
     </div>
--- src/ui/PasswordField.jsx
+++ src/ui/PasswordField.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useId, useState } from "react";
 
 const PasswordField = ({ value, onChange }) => {
   const [showPassword, setShowPassword] = useState(false);
-
+  const id = useId();
   return (
     <div className="relative">
       <input
@@ -13,8 +13,12 @@ const PasswordField = ({ value, onChange }) => {
         onChange={onChange}
         className="peer w-full px-5 py-3 rounded-xl border border-gray-400  placeholder-transparent focus:border-purple-500 focus:ring-1 focus:ring-purple-500 focus:outline-none transition duration-300 focus:text-purple-500 text-gray-500"
         required
+        id={id}
       />
-      <label className="absolute left-5 -top-2.5 text-gray-500 focus:text-purple-600 text-sm font-medium transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-gray-400 peer-placeholder-shown:text-base peer-focus:-top-2.5 peer-focus:text-purple-600 peer-focus:text-sm bg-white px-1">
+      <label
+        htmlFor={id}
+        className="absolute left-5 -top-2.5 text-gray-500 focus:text-purple-600 text-sm font-medium transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-gray-400 peer-placeholder-shown:text-base peer-focus:-top-2.5 peer-focus:text-purple-600 peer-focus:text-sm bg-white px-1"
+      >
         Password
       </label>
       <button
