3038a58107fcdd0a4bd681dcf604ae3d28a9ad91
disha rajput <dishacsevira@gmail.com>
2025-09-27 17:42:41 +0530

    change the back ground color

--- src/components/InputContainer.jsx
+++ src/components/InputContainer.jsx
@@ -8,12 +8,12 @@ export default function InputContainer({
   handleAdd,
 }) {
   return (
-    <div className="flex m-4 gap-2 justify-center mb-4 w-full max-w-md">
+    <div className="flex m-4 gap-2 justify-center mb-4 ">
       <input
         type="text"
         placeholder="Enter a task..."
-        className="flex-1 border border-purple-300 rounded-md px-3 py-2 
-                   outline-none focus:ring-2 focus:ring-purple-300 
+        className="flex-1 border border-purple-200 rounded-md px-3 py-2 
+                   outline-none focus:ring-2 focus:ring-purple-200 
                    bg-white text-purple-700 placeholder:text-purple-300"
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
@@ -26,7 +26,7 @@ export default function InputContainer({
       <button
         onClick={handleAdd}
         aria-label="Add todo"
-        className="px-3 py-2 rounded-md bg-purple-50 text-purple-800 border border-purple-200 
+        className="px-3 py-2 rounded-md bg-purple-50 text-purple-700 border border-purple-200 
                    hover:bg-purple-200 cursor-pointer transition-colors duration-200 
                    flex items-center justify-center"
       >
