9d5741557e227699b7eaa43608a577c9cc0b1bf3
disha rajput <dishacsevira@gmail.com>
2025-09-30 10:30:30 +0530

    change check sign with responsive mode

--- src/components/CustomCheckBox.jsx
+++ src/components/CustomCheckBox.jsx
@@ -2,20 +2,21 @@
 
 const CustomCheckbox = ({ onChange, checked }) => {
   return (
-    <label className="relative">
+    <label className="relative flex items-center">
       <input
         type="checkbox"
         onChange={onChange}
         checked={checked}
         className="w-3 h-3 sm:w-4 sm:h-4 cursor-pointer appearance-none
-        text-center
-          border border-purple-400 rounded
-          checked:bg-purple-400
-          checked:border-purple-400
-          checked:[&::after]:content-['✔']
-          checked:[&::after]:text-purple-700
-          checked:[&::after]:block
-          checked:[&::after]:text-xs"
+               border border-purple-400 rounded
+               checked:bg-purple-400
+               checked:border-purple-400
+               checked:[&::after]:content-['✔']
+               checked:[&::after]:text-purple-700
+               checked:[&::after]:block
+               checked:[&::after]:text-[10px]
+               sm:checked:[&::after]:text-xs
+               flex items-center justify-center"
       />
     </label>
   );
